d02a30f3c6ed4b3e1836aea2197d516790db1324
Ulifly <ulifly@hotmail.com>
2023-07-05 01:01:06 -0600

    json

---
+++ parte8/json-demo.js
@@ -0,0 +1,8 @@
+//importamos el json
+const curso = require('./curso.json');
+
+
+//podemos llamar al json con la notacion de objetos de js
+console.log(curso.temas);
+console.log(curso.esPublico);
+console.log(curso.titulo);
