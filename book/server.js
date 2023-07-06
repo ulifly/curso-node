28eba77e6d835c0862e8c9051a651d6755ea0471
Ulifly <ulifly@hotmail.com>
2023-07-06 12:39:04 -0600

    servidor

---
+++ book/server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, { "content-type": "text/hrml" });
+    response.write("hola Isabela");
+    response.end();
+  })
+  .listen(8888);
